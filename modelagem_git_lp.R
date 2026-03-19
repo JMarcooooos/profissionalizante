5b43875769c37dd99a461145098367acac172c12
JMarcooooos <jmribe3@gmail.com>
2026-03-19 08:50:37 -0300

    Refactor model to use binomial family for LP
    Updated model to use binomial family and adjusted formula.

--- modelagem_git_lp.R
+++ modelagem_git_lp.R
@@ -2,17 +2,18 @@ dados_modelo <- readRDS("dados_modelo.rds")
 
 library(brms)
 
-form_lp <- bf(LP_std ~ 0 + ETAPA + ETAPA:EP_fct + NSE_std + (1 | NM_REGIONAL / CD_ESCOLA))
+form_lp <- bf(
+  ACERTOS_LP | trials(NU_TOTAL) ~ 0 + ETAPA + ETAPA:EP_fct + NSE_std + (1 | NM_REGIONAL / CD_ESCOLA)
+)
 
 modelo_lp <- brm(
   form_lp,
   data = dados_modelo,
-  family = gaussian(),
+  family = binomial(link = "logit"), 
   prior = c(
-    prior(normal(0, 0.5), class = "b"), 
-    prior(exponential(1), class = "sd")
-  ),
-  iter = 2000, warmup = 1000,
+    prior(normal(0, 1.5), class = "b"), 
+    prior(exponential(1), class = "sd")),
+    iter = 2000, warmup = 1000,
   backend = 'cmdstanr',
   cores = 4, chains = 4, threads = threading(1), 
   stan_model_args = list(stanc_options = list("O1"))
