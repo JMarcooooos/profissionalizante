bbe567b550e49d9b29564194fdc3bec21517e3f7
JMarcooooos <jmribe3@gmail.com>
2026-03-23 10:03:39 -0300

    Update modelagem_git_mt.R

--- modelagem_git_mt.R
+++ modelagem_git_mt.R
@@ -2,21 +2,21 @@ dados_modelo <- readRDS("dados_modelo.rds")
 
 library(brms)
 
-form_mt <- bf(
-  ACERTOS_MT | trials(NU_TOTAL) ~ 0 + ETAPA + ETAPA:EP_fct + NSE_std + (1 | NM_REGIONAL / CD_ESCOLA)
-)
+form_mt <- bf(MT_std ~ 0 + ETAPA + ETAPA:EP_fct + (1 | NM_REGIONAL / CD_ESCOLA))
 
 modelo_mt <- brm(
   form_mt,
   data = dados_modelo,
-  family = binomial(link = "logit"), 
+  family = gaussian(),
   prior = c(
-    prior(normal(0, 1.5), class = "b"), 
-    prior(exponential(1), class = "sd")),
+    prior(normal(0, 0.5), class = "b"), 
+    prior(exponential(1), class = "sd")
+  ),
   iter = 2000, warmup = 1000,
   backend = 'cmdstanr',
   cores = 4, chains = 2, threads = threading(1),
   stan_model_args = list(stanc_options = list("O1"))
 )
+
 
 saveRDS(modelo_mt, "modelo_mt_ajustado.rds")
